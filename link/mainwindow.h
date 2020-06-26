2c2494867f2d90f52d3864a477552372e9ca388e
张宇轩 <55274846+tkinter-zyx@users.noreply.github.com>
2020-06-26 11:45:05 +0800

    Add files via upload

---
+++ link/mainwindow.h
@@ -0,0 +1,26 @@
+#include <int_link.h>
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private slots:
+    void on_creat_int_clicked(bool checked);
+
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_H
